6934649372c396347577091a7c916195979452df
joshrado <jradocho@purdue.edu>
2017-03-27 16:10:49 -0400

    first attempt at matching people with the same preference in the same activity. not working currently.

--- Backend/driver.js
+++ Backend/driver.js
@@ -2,11 +2,11 @@ var backend = require('./backend1');
 
 //testing@purdue.edu - testing
 //jradocho@purdue.edu - password
-backend.signIn("testing@purdue.edu", "testing");
+backend.signIn("jradocho@purdue.edu", "password");
 
 //backend.addAuthUser("testing@purdue.edu", "testing", "Test", "Tester", "imatester");
 //backend.initiateConversation("U1PMF6lJBzereWItzrxnQbCrIH03");
-backend.sendMessage("U1PMF6lJBzereWItzrxnQbCrIH03", "This is the sixth message in this conversation.");
+//backend.sendMessage("U1PMF6lJBzereWItzrxnQbCrIH03", "This is the sixth message in this conversation.");
 //console.log(backend.getActivityList());
 
 //backend.viewConversation("U1PMF6lJBzereWItzrxnQbCrIH03");
@@ -14,7 +14,12 @@ backend.sendMessage("U1PMF6lJBzereWItzrxnQbCrIH03", "This is the sixth message i
 
 //backend.addAuthUser("testing@purdue.edu", "testing", "Test", "Tester", "imatester");
 //backend.initiateConversation("U1PMF6lJBzereWItzrxnQbCrIH03");
-backend.sendMessage("U1PMF6lJBzereWItzrxnQbCrIH03", "This is the sixth message in this conversation.");
+//backend.sendMessage("U1PMF6lJBzereWItzrxnQbCrIH03", "This is the 10 message in this conversation.");
+//backend.sendMessage("U1PMF6lJBzereWItzrxnQbCrIH03", "This is the 11 message in this conversation.");
+//backend.sendMessage("U1PMF6lJBzereWItzrxnQbCrIH03", "This is the 12 message in this conversation.");
+//backend.setPreferencesForActivity("Eat", ["No Preference", "Earhart", "Ford", "Wiley", "Hillenbrand", "Windsor"]);
+backend.setPreferencesForUser("Eat", "Earhart");
+
 //console.log(backend.getActivityList());
 
 //backend.viewConversation("U1PMF6lJBzereWItzrxnQbCrIH03");
