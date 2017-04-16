70a85d7d320e8b2d8b855b0f1205af367e82b6b8
Apoorva Parmar <parmar2@purdue.edu>
2017-04-16 15:36:45 -0400

    Updated Sign Up for including Gender

--- Frontend/dist/app/example.js
+++ Frontend/dist/app/example.js
@@ -284,11 +284,12 @@ angular
     .module('example')
     .controller('SignupController', function($scope, supersonic,backendService) {
 
-        $scope.email = "abcd@gmail.com";
+        $scope.email = "apu@gmail.com";
         $scope.password = "helloworld";
         $scope.firstName = "Alpha";
         $scope.lastName = "Numeric";
         $scope.username = "alpha";
+        $scope.gender = "Female";
 
         $scope.signup = function() {
             /* Note: Perform ERROR CHECKING for all fields */
@@ -296,10 +297,11 @@ angular
             .then(function(object) {
                 firebase.auth().onAuthStateChanged(function(currentUser) {
                     if(currentUser) {
-                        firebase.database().ref('users/' + currentUser.uid).set({
-                            firstName: $scope.firstName,
-                            lastName: $scope.lastName,
-                            username: $scope.username
+                        firebase.database().ref('Users/' + currentUser.uid + '/Profile').set({
+                            FirstName: $scope.firstName,
+                            LastName: $scope.lastName,
+                            Username: $scope.username,
+                            Gender: $scope.gender
                         });
                     }
 
